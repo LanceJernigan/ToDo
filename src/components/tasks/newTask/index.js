c96debd5073e24dc1303390f09748383730696af
Lance Jernigan <Lance@LanceJernigan.com>
2017-04-25 11:13:46 -0400

    Minor design clean up (#1)

--- src/components/tasks/newTask/index.js
+++ src/components/tasks/newTask/index.js
@@ -3,9 +3,11 @@ import {connect} from 'react-redux'
 
 import {addTask} from '../actions'
 
+import styles from './style.css'
+
 const NewTask = ({actions, parent}) => {
 
-    return <input onKeyPress={(e) => {
+    return <input className={styles.newTask} placeholder="New Task" onKeyPress={(e) => {
       if (e.charCode === 13) {
         actions.task.add({name: e.currentTarget.value, parent: parent})
         e.currentTarget.value = ''
