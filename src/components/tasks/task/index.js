7bd9174367c00131393b703efc1ade624cac207f
LanceJernigan <lance@knoxweb.com>
2017-04-26 00:55:44 -0400

    Added List Heading

--- src/components/tasks/task/index.js
+++ src/components/tasks/task/index.js
@@ -3,7 +3,7 @@ import {Link} from 'react-router'
 
 import styles from './style.css'
 
-const Task = ({task}) => (
+const Task = (task) => (
   <Link to={`/${task.id}`} className={styles.task} key={task.id}>{task.name}</Link>
 )
 
