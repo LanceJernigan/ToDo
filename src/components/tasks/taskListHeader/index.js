5b735c47e7aa15558786d0fb6ac58afd091d0ed0
Lance Jernigan <Lance@LanceJernigan.com>
2017-04-26 15:36:14 -0400

    Design Cleanup
    * Minor design clean up
    
    * Added List Heading
    
    * Filled out design a little more

--- src/components/tasks/taskListHeader/index.js
+++ src/components/tasks/taskListHeader/index.js
@@ -6,7 +6,7 @@ import styles from './style.css'
 
 const TaskListHeader = ({parent}) => (
   <div className={styles.taskListHeader}>
-    <Link to={`/${parent.hasOwnProperty('parent') && parent.parent !== null ? parent.parent : ''}`}>{parent.hasOwnProperty('name') ? `< Back ${parent.name}` : '< Back'}</Link>
+    <Link to={`/${parent.hasOwnProperty('parent') && parent.parent !== null ? parent.parent : ''}`}>{parent.hasOwnProperty('name') ? parent.name : ''}</Link>
   </div>
 )
 
